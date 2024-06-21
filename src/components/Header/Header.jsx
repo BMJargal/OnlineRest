439b9ba32c0533181804d870e8b55af2a415e41d
BMJargal <batbold.munkhjargal@gmail.com>
2024-06-21 18:55:14 +0200

    Initial commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header-contents">
+        <h2>you can order here</h2>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Non, optio.
+          Atque natus nemo voluptates quidem exercitationem, est corrupti!
+          Laborum quam esse magnam totam, molestias quidem laboriosam. In
+          similique ex illo dolore ad repellat eligendi, tenetur repudiandae
+          quos inventore id? Quas voluptas laborum dolor id asperiores soluta
+          ducimus! Ex, officia voluptatum.
+        </div>
+        <button>View menu</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
